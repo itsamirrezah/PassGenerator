e5c0605c6a1f0e328ca4b7d2827f41155c8e018a
Amirreza H <itsamirrezah@gmail.com>
2020-08-20 00:24:48 +0430

    Extract logic into separate class -refactor

--- app/src/main/java/com/itsamirrezah/passgenerator/MainActivity.kt
+++ app/src/main/java/com/itsamirrezah/passgenerator/MainActivity.kt
@@ -7,9 +7,7 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    val generatedPassword = mutableListOf<String>()
-    val characters = mutableListOf<String>()
-    val symbols = mutableListOf<String>()
+    lateinit var passwordGenerator: PasswordGenerator
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,128 +16,27 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
 
-        //make char with their ascii code
-        makeCharacters()
-        makeSymbols()
 
         btnGenerate.setOnClickListener {
 
             resetValues()
 
-            //how many options (switches) are active
-            var options = uppercase.isChecked.toInt() + sliderSymbols.isChecked.toInt() +
-                    numbers.isChecked.toInt() + 1
-            //read password length from password length slider
-            var pwLength = sliderLength.value.toInt()
+            passwordGenerator = PasswordGenerator(
+                sliderLength.value.toInt(),
+                sliderSymbols.isChecked,
+                uppercase.isChecked,
+                numbers.isChecked
+            )
 
-            //generate n random number (n based on active options)
-            //f.e: if we have 3 active switches we'll get a list<Int> with 4 values
-            val optionQuantity = setupOptionQuantity(pwLength, options)
-            //and later on, we generate final password based on these 4 values
-            //f.e: if we get a list<Int> with these numbers [4,2,6,3]
-            //final password would have 4 symbols, 2 uppercase letter, 6 numbers and 3 lowercase letter
-
-            var index = 0
-            if (sliderSymbols.isChecked) {
-                //generate symbols
-                for (x in 0 until optionQuantity[index++]) {
-                    val randomInt = Random.nextInt(symbols.size)
-                    generatedPassword.add(symbols[randomInt])
-                }
+            passwordGenerator.requestPassword().map {
+                tvPassword.append(it)
             }
-
-            if (uppercase.isChecked) {
-                //generate uppercase letters
-                for (x in 0 until optionQuantity[index++]) {
-                    val randomInt = Random.nextInt(characters.size)
-                    generatedPassword.add(characters[randomInt].toUpperCase())
-                }
-            }
-
-            if (numbers.isChecked) {
-                //generate numbers
-                for (x in 0 until optionQuantity[index++]) {
-                    val randomInt = Random.nextInt(9)
-                    generatedPassword.add(randomInt.toString())
-                }
-            }
-
-            for (x in 0 until optionQuantity[index]) {
-                //generate lowercase letters
-                val randomInt = Random.nextInt(characters.size)
-                generatedPassword.add(characters[randomInt])
-            }
-
-            //shuffle to create final password
-            generatedPassword.shuffle()
-
-            //display final password on the textView
-            generatedPassword.map { tvPassword.append(it) }
-
         }
 
     }
 
-
-    /**
-     * ASCII codes:
-     * ** characters: (a->z) => (97->122)
-     * ** symbols: (33->47), (58->64), (91->96), (123->126)
-     */
-
-    fun makeCharacters() {
-
-        for (x in 97..122) {
-            characters.add(x.toChar().toString())
-        }
-    }
-
-    fun makeSymbols() {
-
-        for (x in 33..47) {
-            symbols.add(x.toChar().toString())
-        }
-
-        for (x in 58..64) {
-            symbols.add(x.toChar().toString())
-        }
-
-        for (x in 91..96) {
-            symbols.add(x.toChar().toString())
-        }
-
-        for (x in 123..126) {
-            symbols.add(x.toChar().toString())
-        }
-    }
-
-    private fun setupOptionQuantity(passwordLength: Int, optionCount: Int): List<Int> {
-
-        val countsByOptions = mutableListOf<Int>()
-
-        var remain: Int
-        for (x in 1 until optionCount) {
-            val optionLeft = optionCount - x
-            remain = passwordLength - optionLeft - countsByOptions.sum()
-            val randomInt = Random.nextInt(1, remain)
-            countsByOptions.add(randomInt)
-        }
-        countsByOptions.add(passwordLength - countsByOptions.sum())
-
-        countsByOptions.shuffle()
-
-        return countsByOptions.toList()
-    }
-
-    fun resetValues() {
-        generatedPassword.clear()
+    private fun resetValues() {
         tvPassword.text = ""
-
     }
-
-
-    //an extension function to convert boolean values to integer
-    fun Boolean.toInt() = if (this) 1 else 0
-
 
 }
---
+++ app/src/main/java/com/itsamirrezah/passgenerator/PasswordGenerator.kt
@@ -0,0 +1,155 @@
+package com.itsamirrezah.passgenerator
+
+import kotlin.random.Random
+
+
+class PasswordGenerator(
+    private var pwLength: Int,
+    private var isUsingSymbols: Boolean,
+    private var isUsingUppercase: Boolean,
+    private var isUsingNumbers: Boolean
+) {
+    private val isUsingLowercase: Boolean = true
+    private val password = mutableListOf<String>()
+    private val letters: List<String> = makeLetters()
+    private val symbols: List<String> = makeSymbols()
+    private val numbers: List<String> = makeNumbers()
+
+    fun requestPassword(): List<String> {
+        //How many options (switches) are active?
+        val options = isUsingUppercase.toInt() + isUsingSymbols.toInt() + isUsingNumbers.toInt() +
+                isUsingLowercase.toInt()
+
+        val optionsQuantity = quantityByOptions(options)
+        generatePassword(optionsQuantity)
+
+        password.shuffle()
+        return password.toList()
+    }
+
+    private fun generatePassword(optionsQuantity: List<Int>) {
+        var index = 0
+
+        if (isUsingUppercase)
+            generatePassword(
+                isUppercase = true,
+                isSymbols = false,
+                isNumber = false,
+                quantity = optionsQuantity[index++]
+            )
+
+        if (isUsingLowercase)
+            generatePassword(
+                isUppercase = false,
+                isSymbols = false,
+                isNumber = false,
+                quantity = optionsQuantity[index++]
+            )
+
+        if (isUsingSymbols)
+            generatePassword(
+                isUppercase = false,
+                isSymbols = true,
+                isNumber = false,
+                quantity = optionsQuantity[index++]
+            )
+
+        if (isUsingNumbers)
+            generatePassword(
+                isUppercase = false,
+                isSymbols = false,
+                isNumber = true,
+                quantity = optionsQuantity[index]
+            )
+
+    }
+
+    private fun generatePassword(
+        isUppercase: Boolean,
+        isSymbols: Boolean,
+        isNumber: Boolean,
+        quantity: Int
+    ) {
+        val list = if (isSymbols) symbols else if (isNumber) numbers else letters
+
+        for (x in 0 until quantity) {
+            val randomInt = Random.nextInt(list.size)
+            var element = list[randomInt]
+            if (isUppercase)
+                element = element.toUpperCase()
+            //put that element into final password
+            password.add(element)
+        }
+
+
+    }
+
+    //Generate N random value (N= number of active options + 1)
+    //f.e: if we have (3) active options, => then we'll get a list<Int> with (4) values
+    //and later on, we generate the final password based on these 4 values
+    //f.e: if we get a list<Int> with these numbers: [4,2,6,3]
+    //the final password should have 4 symbols, 2 uppercase letter, 6 numbers and 3 lowercase letter
+    private fun quantityByOptions(options: Int): List<Int> {
+
+        val optionsQuantity = mutableListOf<Int>()
+
+        var limit: Int
+        for (x in 1 until options) {
+            val opLeft = options - x
+            limit = pwLength - opLeft - optionsQuantity.sum()
+            val randomInt = Random.nextInt(1, limit)
+            optionsQuantity.add(randomInt)
+        }
+        //put last value into the list
+        optionsQuantity.add(pwLength - optionsQuantity.sum())
+
+        optionsQuantity.shuffle()
+
+        return optionsQuantity
+    }
+
+    private fun Boolean.toInt() = if (this) 1 else 0
+
+    /** ASCII:
+     * ** letters: (a->z) => (97->122)
+     * ** symbols: (33->47), (58->64), (91->96), (123->126) */
+    companion object {
+        private fun makeLetters(): List<String> {
+            val letters = mutableListOf<String>()
+            for (x in 97..122) {
+                letters.add(x.toChar().toString())
+            }
+            return letters.toList()
+        }
+
+        private fun makeSymbols(): List<String> {
+            val symbols = mutableListOf<String>()
+            for (x in 33..47) {
+                symbols.add(x.toChar().toString())
+            }
+
+            for (x in 58..64) {
+                symbols.add(x.toChar().toString())
+            }
+
+            for (x in 91..96) {
+                symbols.add(x.toChar().toString())
+            }
+
+            for (x in 123..126) {
+                symbols.add(x.toChar().toString())
+            }
+
+            return symbols
+
+        }
+
+        private fun makeNumbers(): List<String> {
+            val numbers = mutableListOf<String>()
+            for (x in 0..9) numbers.add(x.toString())
+            return numbers
+
+        }
+    }
+
+}
