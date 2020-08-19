8aeac1e50a0c5cdb7c1de2c12107505efa40b58d
Amirreza H <itsamirrezah@gmail.com>
2020-08-19 21:12:28 +0430

    Implement generated password

--- app/src/main/java/com/itsamirrezah/passgenerator/MainActivity.kt
+++ app/src/main/java/com/itsamirrezah/passgenerator/MainActivity.kt
@@ -3,9 +3,11 @@ package com.itsamirrezah.passgenerator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    val generatedPassword = mutableListOf<String>()
     val characters = mutableListOf<String>()
     val symbols = mutableListOf<String>()
 
@@ -19,6 +21,63 @@ class MainActivity : AppCompatActivity() {
         //make char with their ascii code
         makeCharacters()
         makeSymbols()
+
+        btnGenerate.setOnClickListener {
+
+            resetValues()
+
+            //how many options (switches) are active
+            var options = uppercase.isChecked.toInt() + sliderSymbols.isChecked.toInt() +
+                    numbers.isChecked.toInt() + 1
+            //read password length from password length slider
+            var pwLength = sliderLength.value.toInt()
+
+            //generate n random number (n based on active options)
+            //f.e: if we have 3 active switches we'll get a list<Int> with 4 values
+            val optionQuantity = setupOptionQuantity(pwLength, options)
+            //and later on, we generate final password based on these 4 values
+            //f.e: if we get a list<Int> with these numbers [4,2,6,3]
+            //final password would have 4 symbols, 2 uppercase letter, 6 numbers and 3 lowercase letter
+
+            var index = 0
+            if (sliderSymbols.isChecked) {
+                //generate symbols
+                for (x in 0 until optionQuantity[index++]) {
+                    val randomInt = Random.nextInt(symbols.size)
+                    generatedPassword.add(symbols[randomInt])
+                }
+            }
+
+            if (uppercase.isChecked) {
+                //generate uppercase letters
+                for (x in 0 until optionQuantity[index++]) {
+                    val randomInt = Random.nextInt(characters.size)
+                    generatedPassword.add(characters[randomInt].toUpperCase())
+                }
+            }
+
+            if (numbers.isChecked) {
+                //generate numbers
+                for (x in 0 until optionQuantity[index++]) {
+                    val randomInt = Random.nextInt(9)
+                    generatedPassword.add(randomInt.toString())
+                }
+            }
+
+            for (x in 0 until optionQuantity[index]) {
+                //generate lowercase letters
+                val randomInt = Random.nextInt(characters.size)
+                generatedPassword.add(characters[randomInt])
+            }
+
+            //shuffle to create final password
+            generatedPassword.shuffle()
+
+            //display final password on the textView
+            generatedPassword.map { tvPassword.append(it) }
+
+        }
+
     }
 
 
@@ -53,5 +112,34 @@ class MainActivity : AppCompatActivity() {
             symbols.add(x.toChar().toString())
         }
     }
+
+    private fun setupOptionQuantity(passwordLength: Int, optionCount: Int): List<Int> {
+
+        val countsByOptions = mutableListOf<Int>()
+
+        var remain: Int
+        for (x in 1 until optionCount) {
+            val optionLeft = optionCount - x
+            remain = passwordLength - optionLeft - countsByOptions.sum()
+            val randomInt = Random.nextInt(1, remain)
+            countsByOptions.add(randomInt)
+        }
+        countsByOptions.add(passwordLength - countsByOptions.sum())
+
+        countsByOptions.shuffle()
+
+        return countsByOptions.toList()
+    }
+
+    fun resetValues() {
+        generatedPassword.clear()
+        tvPassword.text = ""
+
+    }
+
+
+    //an extension function to convert boolean values to integer
+    fun Boolean.toInt() = if (this) 1 else 0
+
 
 }
